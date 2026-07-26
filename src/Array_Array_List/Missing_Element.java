ba0a5e120d271f4315a699d3f2541a446ba88166
satvik31-hash <satviksaxena31@gmail.com>
2026-07-27 00:16:28 +0530

    Solve Missing Element problem using sum formula

--- src/Array_Array_List/Missing_Element.java
+++ src/Array_Array_List/Missing_Element.java
@@ -1,4 +1,20 @@
 package Array_Array_List;
-
+import java.util.Scanner;
 public class Missing_Element {
+    static void main() {
+         int arr[] = {8,2,4,5,3,7,1};
+         Scanner scanner = new Scanner(System.in);
+         int n = scanner.nextInt();
+         int sum = (n*(n+1))/2;
+         int sum1=0;
+         for(int i = 0; i<arr.length;i++){
+             sum1+=arr[i];
+         }
+         if(sum==sum1){
+             System.out.println("No missing element found");
+         }
+         else{
+             System.out.print("Missing Number is:- "+ (sum-sum1));
+         }
+    }
 }
--- src/Array_Array_List/Two_Sum.java
+++ src/Array_Array_List/Two_Sum.java
@@ -1,4 +1,19 @@
 package Array_Array_List;
 
 public class Two_Sum {
+    static void main() {
+        //Given Array
+        int[] a = {1,3,-3,5,4};
+        int sum = 2;
+        int b =1;
+        for(int i = 0;i<a.length;i++){
+            int point = a[i];
+            for(int j =b;j<a.length;j++){
+                if(point+a[j]==sum){
+                    System.out.println("Two elements whose sum is 2 are "+point+" and "+a[j]);
+                }
+            }
+            b+=1;
+        }
+    }
 }
