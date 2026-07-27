0c69972a6e71e0195be981cbd2320956d5a1ca84
satvik31-hash <satviksaxena31@gmail.com>
2026-07-27 13:31:29 +0530

    Solve Missing Element problem using sum formula

--- src/Array_Array_List/Missing_Element.java
+++ src/Array_Array_List/Missing_Element.java
@@ -1,9 +1,13 @@
+/*You are given an Array arr[] of size n-1 with n distinct elements integers in the range from 1 to n(inclusive), This
+array represents a permutation of the integers from 1 to n with one missing element. Your task is to identify and return
+the missing element*/
 package Array_Array_List;
 import java.util.Scanner;
 public class Missing_Element {
     static void main() {
          int arr[] = {8,2,4,5,3,7,1};
          Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter n = '8':-  ");
          int n = scanner.nextInt();
          int sum = (n*(n+1))/2;
          int sum1=0;
